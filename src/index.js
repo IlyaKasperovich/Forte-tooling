2901b80e2432d36d99e26ad135b5762011d750f9
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-06-11 22:57:53 +0300

    asset setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('hello');
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = ['Hello', 'webpack'].join(' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
