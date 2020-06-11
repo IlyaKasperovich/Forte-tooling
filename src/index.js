b52e0b913f403edc245d1d3bd22ef888b13599e7
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-06-11 23:49:56 +0300

    output config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import printMe from './print.js';
+
 import './style.css';
 import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
+  
 
   element.innerHTML = ['Hello', 'webpack'].join(' ');
   element.classList.add('hello');
@@ -10,6 +13,11 @@ function component() {
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
